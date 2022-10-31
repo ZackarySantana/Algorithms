76b3e332c98a5dad3b78a3266cf388b3c15781dc
ZackarySantana <zackzackyack@gmail.com>
2022-10-31 17:53:13 -0400

    Upgraded script to accept languages and or tests

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source utils.sh
+source utils/constants.sh
 
 # ts_bench ${folder name}
 function ts_bench {
@@ -10,21 +10,57 @@ function ts_bench {
     echo -e "${GREEN}Finished Typescript benchmarks (inside ${benchmark_dir})${RESET}"
 }
 
+# run_benchmark ${Language} ${Benchmark name} ${Benchmark command}
+function run_benchmark {
+    echo -e "${YELLOW}Running $1 ${RESET}${2}${YELLOW} benchmarks${RESET}"
+    typescript_benchmark=$($3 | sed "s/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g")
+    echo "${typescript_benchmark}" > "${benchmark_dir}/${2}.${1,,}.log"
+    echo -e "${GREEN}Finished ${1} benchmarks (inside ${benchmark_dir})${RESET}"
+}
+
 mkdir -p ${benchmark_dir}
+
+all=1
+typescript=0
+java=0
 
 function benchmarks {
     for benchmark in "$@" 
     do
         if [[ " ${types[*]} " =~ " ${benchmark} " ]]; then
-            ts_bench $benchmark
+            if [ $all -eq 1 ] || [ $typescript -eq 1 ]; then
+                run_benchmark "Typescript" "${benchmark}" "deno bench --unstable ${benchmark}/benchmarks"
+            fi
         else
-            echo -e "${RED}Skipping $benchmark. NOT FOUND${RESET}"
+            echo -e "${RED}NOT FOUND: Skipping ${benchmark}.${RESET}"
         fi
     done
 }
 
+while getopts ":l:" opt; do
+    case $opt in
+        l)
+            echo -e "${YELLOW}Only testing languages: '$OPTARG'${RESET}"
+            all=0
+            for i in $OPTARG; do
+                if [ ${i,,} = "typescript" ]; then
+                    typescript=1
+                elif [ ${i,,} = "java" ]; then
+                    java=1
+                fi
+            done
+        ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+        ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
 if [ $# -eq 0 ]; then
     benchmarks ${types[@]}
-else 
+else
     benchmarks $@
 fi
--- test.sh
+++ test.sh
@@ -1,32 +1,61 @@
 #!/bin/bash
 
-source utils.sh
+source utils/constants.sh
 
-# run_test ${Test name} ${Test command} ${Failed keyword}
+# run_test ${Language} ${Test name} ${Test command} ${Failed keyword}
 function run_test {
-    echo -e "${YELLOW}Running $1 tests${RESET}"
-    test_results=$($2)
-    if [[ $test_results == *"$3 ("* ]]; then
+    echo -e "${YELLOW}Running $1 ${RESET}$2${YELLOW} tests${RESET}"
+    test_results=$($3)
+    if [[ $test_results == *"$4 ("* ]]; then
       echo -e "${RED}$1 test failed: ${RESET}"
-      echo -e "$test_results" | grep $3
+      echo -e "$test_results" | grep $4
+      echo ""
     else
-        echo -e "${GREEN}$1 tests passed!${RESET}"
+        echo -e "${GREEN}$1 ${RESET}$2${GREEN} tests passed!${RESET}\n"
     fi
 }
+
+all=1
+typescript=0
+java=0
 
 function tests {
     for test in "$@" 
     do
         if [[ " ${types[*]} " =~ " ${test} " ]]; then
-            run_test "Typescript $test" "deno test $test/tests" "FAILED ("
+            if [ $all -eq 1 ] || [ $typescript -eq 1 ]; then
+                run_test "Typescript" "${test}" "deno test ${test}/tests" "FAILED ("
+            fi
         else
-            echo -e "${RED}Skipping $benchmark. NOT FOUND${RESET}"
+            echo -e "${RED}NOT FOUND: Skipping ${test}.${RESET}"
         fi
     done
 }
 
+while getopts ":l:" opt; do
+    case $opt in
+        l)
+            echo -e "${YELLOW}Only testing languages: '$OPTARG'${RESET}"
+            all=0
+            for i in $OPTARG; do
+                if [ ${i,,} = "typescript" ]; then
+                    typescript=1
+                elif [ ${i,,} = "java" ]; then
+                    java=1
+                fi
+            done
+        ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+        ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
 if [ $# -eq 0 ]; then
     tests ${types[@]}
-else 
-    run_test "Typescript" "deno test" "FAILED ("
+else
+    tests $@
 fi

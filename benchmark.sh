fa93d659dc33aad32a10573cd095ba153e9ab279
ZackarySantana <zackzackyack@gmail.com>
2022-10-31 15:02:09 -0400

    Added some automatic tests and benchmark scripts

---
+++ benchmark.sh
@@ -0,0 +1,24 @@
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;33m'
+YELLOW='\033[1;33m'
+RESET='\033[0;0m'
+
+benchmarks="./benchmark_results/"
+
+mkdir -p $benchmarks
+
+for benchmark in "$@" 
+do
+    echo -e "${YELLOW}Running Typescript \"${RED}${benchmark}${YELLOW}\" benchmarks${RESET}"
+    typescript_benchmark=$(deno bench --unstable $benchmark/benchmarks/)
+    typescript_benchmark > "$benchmarks/${benchmark}.ts"
+done
